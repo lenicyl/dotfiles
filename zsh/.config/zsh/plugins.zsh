222ff98c022ecb697626be239dfff4e91be445ee
Lenicyl <73347143+lenicyl@users.noreply.github.com>
2024-04-12 19:25:10 +0530

    zsh: clean up code

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -1,12 +1,10 @@
 ZPLUGDIR="$HOME/.local/share/zsh"
 
-[ ! -d $ZPLUGDIR/fsh ] && {
+[ ! -d $ZPLUGDIR/fsh ] && 
   git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting $ZPLUGDIR/fsh
-}
 
-[ ! -d $ZPLUGDIR/zsh-autosuggestions ] && {
+[ ! -d $ZPLUGDIR/zsh-autosuggestions ] && 
   git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions $ZPLUGDIR/zsh-autosuggestions
-}
 
 autoload -U zrecompile
 for plug in $ZPLUGDIR/{fsh/fast-*,zsh-autosuggestions/{zsh-autosuggestions.zsh,src/**/*.zsh}}; do
