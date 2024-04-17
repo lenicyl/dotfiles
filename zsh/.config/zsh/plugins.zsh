684b786a4063c6afd848b2e0bdee8bdd5ebe5a91
Lenicyl <73347143+lenicyl@users.noreply.github.com>
2024-04-17 11:44:23 +0530

    zsh: Fix fsf compilation
    Taken from zsh-bench configs

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -1,16 +1,18 @@
 ZPLUGDIR="$HOME/.local/share/zsh"
 
 [ ! -d $ZPLUGDIR/fsh ] && {
-  git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting $ZPLUGDIR/fsh
-  zcompile-many $ZPLUGDIR/fsh/fast-* 
+	git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting $ZPLUGDIR/fsh
+	mv -- $ZPLUGDIR/fsh/{'→chroma','tmp'}
+	zcompile-many $ZPLUGDIR/fsh/{fast*,.fast*,**/*.ch,**/*.zsh}
+	mv -- $ZPLUGDIR/fsh/{'tmp','→chroma'}
 }
 
 [ ! -d $ZPLUGDIR/zsh-autosuggestions ] && {
-  git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions $ZPLUGDIR/zsh-autosuggestions
-  zcompile-many $ZPLUGDIR/zsh-autosuggestions/{zsh-autosuggestions.zsh,src/**/*.zsh}
+	git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions $ZPLUGDIR/zsh-autosuggestions
+	zcompile-many $ZPLUGDIR/zsh-autosuggestions/{zsh-autosuggestions.zsh,src/**/*.zsh}
 }
 unfunction zcompile-many
 
 ZSH_AUTOSUGGEST_MANUAL_REBIND=1  
-source $ZPLUGDIR/fsh/fast-syntax-highlighting.plugin.zsh
+source $ZPLUGDIR/fsh/fast-syntax-highlighting.plugin.zsh && fast-theme XDG:catppuccin-mocha > /dev/null 
 source $ZPLUGDIR/zsh-autosuggestions/zsh-autosuggestions.zsh
