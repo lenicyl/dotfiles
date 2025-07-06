b91b4a4b4ecb68b8106931d9b09d0af9b84e2176
Lenicyl <lenicyl@disroot.org>
2025-07-06 17:22:36 +0530

    Shell: Add environment variables file for all shells
    Still need to modify zsh to use the env.sh file
    Also need to combine the alias files from zsh folder

---
+++ shell/.config/shell/env.sh
@@ -0,0 +1,16 @@
+### Environment variables for all shells
+
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+
+export CARGO_HOME="$HOME/.local/src/cargo"
+export RUSTUP_HOME="$HOME/.local/src/rustup"
+export PATH="$HOME/.local/bin:$CARGO_HOME/bin:$PATH"
+
+# Programs
+export EDITOR="hx"
+export BROWSER="firefox"
+
+# I dont use X11 but sddm does
+XCURSOR_SIZE=64
