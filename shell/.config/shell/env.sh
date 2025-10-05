674a0d195247c34d5960b6db0f4d565c945bdcfd
Lenicyl <lenicyl@disroot.org>
2025-10-05 10:08:14 +0530

    shell: add flutter and android env variables

--- shell/.config/shell/env.sh
+++ shell/.config/shell/env.sh
@@ -8,6 +8,12 @@ export CARGO_HOME="$HOME/.local/src/cargo"
 export RUSTUP_HOME="$HOME/.local/src/rustup"
 export PATH="$HOME/.local/bin:$CARGO_HOME/bin:$PATH"
 
+export ANDROID_SDK_ROOT="$HOME/.local/dev/android"
+export PATH="$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$PATH"
+
+export FLUTTER_HOME="$HOME/.local/dev/flutter"
+export PATH="$FLUTTER_HOME/bin:$PATH"
+
 # Programs
 export EDITOR="hx"
 export BROWSER="firefox"
